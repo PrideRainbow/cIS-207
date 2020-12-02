45b11de8c75edde2899c4e23189192e7bf92ee63
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2020-12-02 06:24:23 +0000

    More information

--- StockSimulator/Core/Accounts/Views/AccountRow.swift
+++ StockSimulator/Core/Accounts/Views/AccountRow.swift
@@ -2,4 +2,13 @@
 //  AccountRow.swift
 //  StockSimulator
 //
-//  Created b
+//  Created by Christopher Walter on 2/26/22.
+//
+
+import SwiftUI
+
+struct AccountRow: View {
+    
+    @Environment(\.managedObjectContext) var moc // CoreData
+    
+    var vm: Acc
