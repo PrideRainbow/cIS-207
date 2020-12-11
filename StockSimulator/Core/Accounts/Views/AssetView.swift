b7d0d70f17081089bb4998e051143781da4b449d
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2020-12-11 07:58:09 +0000

    hwmon: (applesmc) Limit key length in warning messages

--- StockSimulator/Core/Accounts/Views/AssetView.swift
+++ StockSimulator/Core/Accounts/Views/AssetView.swift
@@ -1,2 +1,13 @@
 //
+//  AssetView.swift
+//  StockSimulator
 //
+//  Created by Christopher Walter on 3/16/22.
+//
+
+import SwiftUI
+
+struct AssetView: View {
+    
+    // will allow us to dismiss
+    @
