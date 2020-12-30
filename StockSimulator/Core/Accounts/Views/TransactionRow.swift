5f4626420691923a5ace369e48cef5643d5efc19
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2020-12-30 20:43:17 +0000

    Rename file

--- StockSimulator/Core/Accounts/Views/TransactionRow.swift
+++ StockSimulator/Core/Accounts/Views/TransactionRow.swift
@@ -9,4 +9,10 @@ import SwiftUI
 
 struct TransactionRow: View {
     
-    var transaction: Transact
+    var transaction: Transaction
+    var body: some View {
+        VStack {
+            HStack {
+                Text("\(transaction.eventType ?? "Unknown"): ")
+                    .font(.headline)
+      
