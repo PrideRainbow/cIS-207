80fb484eb5d83ec39c3675d906a11a857e29cc53
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-01-02 12:59:42 +0000

    Formatting issue

--- StockSimulator/Core/Charts/Views/BarView.swift
+++ StockSimulator/Core/Charts/Views/BarView.swift
@@ -2,4 +2,14 @@
 //  BarView.swift
 //  StockSimulator
 //
-//  Created by Christopher W
+//  Created by Christopher Walter on 8/27/22.
+//
+
+import SwiftUI
+
+struct BarView: View {
+    var name: String
+    var value: Double // estimate
+    var maxValue: Double
+    var minValue: Double
+    var totalHeight: Double 
