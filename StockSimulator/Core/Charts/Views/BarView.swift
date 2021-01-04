93ecd54d15c61c479861aedeae0d89d3366f6435
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-01-04 20:15:57 +0000

    Comment removed

--- StockSimulator/Core/Charts/Views/BarView.swift
+++ StockSimulator/Core/Charts/Views/BarView.swift
@@ -12,4 +12,13 @@ struct BarView: View {
     var value: Double // estimate
     var maxValue: Double
     var minValue: Double
-    var totalHeight: Double 
+    var totalHeight: Double // total height of graph
+    
+    var color: Color
+
+    var range: Double {
+        if minValue >= 0 {
+            return maxValue
+        }
+        else if maxValue <= 0 {
+            
