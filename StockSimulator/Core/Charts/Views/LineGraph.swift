b8d90404695abf52c4f0c48754cc56598e0ae346
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-01-15 16:13:46 +0000

    update bower

--- StockSimulator/Core/Charts/Views/LineGraph.swift
+++ StockSimulator/Core/Charts/Views/LineGraph.swift
@@ -2,4 +2,13 @@
 //  LineGraph.swift
 //  StockSimulator
 //
-//  Created by C
+//  Created by Christopher Walter on 3/24/22.
+//
+
+import SwiftUI
+
+struct LineGraph: Shape {
+    // These are the normalized Data Points between 0 and 1
+    var dataPoints: [Double]
+
+    func path(in rect: CGRect) -> Path 
