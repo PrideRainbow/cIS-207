e8edc0fc56ba64c8455f21ccf9f804e2a4e881ce
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-01-16 21:56:09 +0000

    sb_edac: Fix erroneous bytes->gigabytes conversion

--- StockSimulator/Core/Charts/Views/LineGraph.swift
+++ StockSimulator/Core/Charts/Views/LineGraph.swift
@@ -11,4 +11,9 @@ struct LineGraph: Shape {
     // These are the normalized Data Points between 0 and 1
     var dataPoints: [Double]
 
-    func path(in rect: CGRect) -> Path 
+    func path(in rect: CGRect) -> Path {
+
+        func point(at ix: Int) -> CGPoint {
+            let point = dataPoints[ix]
+            let x = rect.width * CGFloat(ix) / CGFloat(dataPoints.count - 1)
+            let y = (1 - CGFloat(
