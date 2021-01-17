7e4e2ca2e1abd73082ac1fb627f523d3c64cdacb
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-01-17 22:34:37 +0000

    add missing migrations

--- StockSimulator/Core/Home/ViewModels/MarketSummaryViewModel.swift
+++ StockSimulator/Core/Home/ViewModels/MarketSummaryViewModel.swift
@@ -1,2 +1,13 @@
 //
-//  MarketSummaryViewModel.s
+//  MarketSummaryViewModel.swift
+//  StockSimulator
+//
+//  Created by Christopher Walter on 9/9/22.
+//
+
+import Foundation
+import SwiftUI
+
+class MarketSummaryViewModel: ObservableObject
+{
+    @Published var marketDat
