6b2bf9ce4da44237563868cdb96d722f71ea90d9
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-07-11 20:27:03 +0000

    Version 1.1r58

--- StockSimulator/Core/Launch/Views/LaunchView.swift
+++ StockSimulator/Core/Launch/Views/LaunchView.swift
@@ -8,4 +8,10 @@
 import SwiftUI
 
 struct LaunchView: View {
-    @State private var loadingText: [String] = "Loading your portfolio...".map { String($0) 
+    @State private var loadingText: [String] = "Loading your portfolio...".map { String($0) }
+    
+    @State private var showLoadingText: Bool = false
+    
+    let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    
+    @State private var counter: I
