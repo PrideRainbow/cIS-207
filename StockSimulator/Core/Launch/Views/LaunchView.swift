9585c57b9096f76a228111bef32998a014482004
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-07-11 12:11:53 +0000

    Update about.html.erb

--- StockSimulator/Core/Launch/Views/LaunchView.swift
+++ StockSimulator/Core/Launch/Views/LaunchView.swift
@@ -1,2 +1,11 @@
 //
-// 
+//  LaunchView.swift
+//  StockSimulator
+//
+//  Created by Christopher Walter on 8/16/22.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    @State private var loadingText: [String] = "Loading your portfolio...".map { String($0) 
