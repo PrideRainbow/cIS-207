c5a994ecbfa4125e3ba1287e5605c4e996b71cf4
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-07-23 13:12:54 +0000

    Clean code.

--- StockSimulator/Core/Stocks/ViewModels/StockDetailViewModel.swift
+++ StockSimulator/Core/Stocks/ViewModels/StockDetailViewModel.swift
@@ -5,4 +5,13 @@
 //  Created by Christopher Walter on 7/27/22.
 //
 
-import Foun
+import Foundation
+import SwiftUI
+import Combine
+
+class StockDetailViewModel: ObservableObject
+{
+    @Published var symbol: String = ""
+//    @Published var stock: Stock? = nil
+    
+    @Published va
