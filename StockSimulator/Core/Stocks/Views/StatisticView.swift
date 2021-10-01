aaea99810e8c7a39930cef6465cbb1ad9d4b0735
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-10-01 15:14:09 +0000

    Add slides

--- StockSimulator/Core/Stocks/Views/StatisticView.swift
+++ StockSimulator/Core/Stocks/Views/StatisticView.swift
@@ -1,61 +1,53 @@
+
 //
-//  StatisticRow.swift
+//  StatisticView.swift
 //  StockSimulator
 //
-//  Created by Christopher Walter on 8/26/22.
+//  Created by Christopher Walter on 7/24/22.
 //
 
 import SwiftUI
 
-struct StatisticRow: View {
+struct StatisticView: View {
     
     let stat: StatisticModel
     
     var body: some View {
-        HStack(spacing: 15) {
+        VStack(alignment: .leading, spacing: 4) {
             Text(stat.title)
                 .font(.caption)
-                .lineLimit(2)
-//                .foregroundColor(Color.theme.secondaryText)
-            Spacer()
-            
+                .foregroundColor(Color.theme.secondaryText)
+            Text(stat.value)
+                .font(.headline)
+                .foregroundColor(Color.theme.accent)
             HStack {
                 Image(systemName: "triangle.fill")
                     .font(.caption2)
                     .rotationEffect(
                         Angle(degrees:(stat.percentageChange ?? 0) >= 0 ? 0 : 180))
-                
                 Text((stat.isPercentChange ?? true) ? (stat.percentageChange?.asPercentString() ?? ""): (stat.percentageChange?.formattedWithAbbreviations() ?? ""))
                     .font(.caption)
                     .bold()
-                    .lineLimit(2)
             }
             .foregroundColor((stat.percentageChange ?? 0) >= 0 ? Color.theme.green : Color.theme.red)
             .opacity(stat.percentageChange == nil ? 0.0 : 1.0)
-            Text(stat.value)
-                .font(.caption)
-                .foregroundColor(Color.theme.accent)
-                .lineLimit(2)
-            
             
         }
-    
-        
     }
 }
 
-struct StatisticRow_Previews: PreviewProvider {
+struct StatisticView_Previews: PreviewProvider {
     static var previews: some View {
-        VStack {
-            StatisticRow(stat: dev.stat1)
-//                .previewLayout(.sizeThatFits)
+        Group {
+            StatisticView(stat: dev.stat1)
+                .previewLayout(.sizeThatFits)
                 .preferredColorScheme(.dark)
-            StatisticRow(stat: dev.stat2)
-//                .previewLayout(.sizeThatFits)
-            StatisticRow(stat: dev.stat3)
-//                .previewLayout(.sizeThatFits)
+            StatisticView(stat: dev.stat2)
+                .previewLayout(.sizeThatFits)
+            StatisticView(stat: dev.stat3)
+                .previewLayout(.sizeThatFits)
                 .preferredColorScheme(.dark)
         }
-
+        
     }
 }
---
+++ StockSimulator/Core/Stocks/Views/StockBasicView.swift
@@ -0,0 +1,10 @@
+//
+//  StockView.swift
+//  StockSimulator
+//
+//  Created by Christopher Walter on 1/29/22.
+//
+
+import SwiftUI
+
+struct StockBasicView: View {
