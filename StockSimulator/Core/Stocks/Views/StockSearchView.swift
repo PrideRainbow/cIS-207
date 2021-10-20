36c4a8f7561e730b58cc4b16a4720ee16592137a
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-10-20 18:58:50 +0000

    adding classes

--- StockSimulator/Core/Stocks/Views/StockSearchView.swift
+++ StockSimulator/Core/Stocks/Views/StockSearchView.swift
@@ -1,2 +1,14 @@
 //
 //  StockSearchView.swift
+//  StockSimulator
+//
+//  Created by Christopher Walter on 2/12/22.
+//
+
+import SwiftUI
+
+struct StockSearchView: View {
+    
+    @ObservedObject var vm = StocksViewModel()
+    
+    @Enviro
