70e5b8ab47ae8222cf7e25931a009aff3e514bef
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-11-07 10:08:47 +0000

    fix

--- StockSimulator/Core/Watchlists/Views/AddWatchlistView.swift
+++ StockSimulator/Core/Watchlists/Views/AddWatchlistView.swift
@@ -1,2 +1,13 @@
 //
-/
+//  AddWatchlistView.swift
+//  StockSimulator
+//
+//  Created by Christopher Walter on 3/4/22.
+//
+
+import SwiftUI
+
+struct AddWatchlistView: View {
+    @State var name: String
+    
+    @Env
