5d1a98bd3d334f9087fc5f3534472de993015243
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-11-28 08:28:36 +0000

    Update some deps

--- StockSimulator/Extensions/Array.swift
+++ StockSimulator/Extensions/Array.swift
@@ -8,4 +8,4 @@
 import Foundation
 import SwiftUI
 
-// This is used to normalize the data in an a
+// This is used to normalize the data in an array so that we can plot it on the graph. Data is all over the place and change the values to all be between 0 and 1. 0 in min, 1 is max. might have to change this to 0.1 and 0.9 in the future, so that we have some whi
