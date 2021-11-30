febd735281761850a7a1afe683c7f5ad5b7a9b3e
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-11-30 19:41:31 +0000

    #30

--- StockSimulator/Extensions/Array.swift
+++ StockSimulator/Extensions/Array.swift
@@ -12,4 +12,9 @@ import SwiftUI
 extension Array where Element == Double {
     // Return the elements of the sequence Normalized
     var normalized: [Double] {
-     
+        if let min = self.min(), let max = self.max() {
+            return self.map { ($0 - min) / ( max - min) }
+        }
+        else {
+            // we do not have a minimum and maximum
+            return []
