634d1194e94eb1014531e0c1f4f1d95dba785423
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-11-30 06:45:07 +0000

    Style update

--- StockSimulator/Extensions/Array.swift
+++ StockSimulator/Extensions/Array.swift
@@ -8,4 +8,8 @@
 import Foundation
 import SwiftUI
 
-// This is used to normalize the data in an array so that we can plot it on the graph. Data is all over the place and change the values to all be between 0 and 1. 0 in min, 1 is max. might have to change this to 0.1 and 0.9 in the future, so that we have some whi
+// This is used to normalize the data in an array so that we can plot it on the graph. Data is all over the place and change the values to all be between 0 and 1. 0 in min, 1 is max. might have to change this to 0.1 and 0.9 in the future, so that we have some white space at the top and bottom of the chart
+extension Array where Element == Double {
+    // Return the elements of the sequence Normalized
+    var normalized: [Double] {
+     
