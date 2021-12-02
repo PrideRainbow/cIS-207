b4109d4d9efc2013a480ac8533b6f94b16c68dcd
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-12-02 19:29:22 +0000

    fix TODO

--- StockSimulator/Extensions/Color.swift
+++ StockSimulator/Extensions/Color.swift
@@ -2,4 +2,19 @@
 //  Color.swift
 //  StockSimulator
 //
-//  Created by Christopher Walter 
+//  Created by Christopher Walter on 4/14/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    static let launch = LaunchTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let
