451bfa37dbc1f74c7b2a44061cdcabdfcbc80aac
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-12-02 23:09:46 +0000

    RBAC

--- StockSimulator/Extensions/Color.swift
+++ StockSimulator/Extensions/Color.swift
@@ -17,4 +17,8 @@ extension Color {
 
 struct ColorTheme {
     
-    let
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextC
