81f8327db88067ad13726b6e03062360e8a2c10d
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-12-05 19:59:12 +0000

    BUILD-INERA: Set explicit released versions

--- StockSimulator/Extensions/Color.swift
+++ StockSimulator/Extensions/Color.swift
@@ -32,4 +32,13 @@ struct ColorTheme2 {
     let background = Color(#colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1))
     let green = Color(#colorLiteral(red: 0, green: 0.5603182912, blue: 0, alpha: 1))
     let red = Color(#colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1))
-    let secondaryText = Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.754
+    let secondaryText = Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1))
+}
+
+struct LaunchTheme {
+    
+    let accent = Color("LaunchAccentColor")
+    let background = Color("LaunchBackgroundColor")
+    
+}
+
