a6f3c418a98ffb709f82af56e2573799010975bc
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-12-07 18:00:18 +0000

    add comments

--- StockSimulator/Extensions/Date.swift
+++ StockSimulator/Extensions/Date.swift
@@ -1,3 +1,16 @@
 //
 //  Date.swift
-//  StockSimulat
+//  StockSimulator
+//
+//  Created by Christopher Walter on 5/18/22.
+//
+
+import Foundation
+
+
+extension Date
+{
+    
+    // convert a time since 1970 into a date to be displayed
+//    init()
+    private var sh
