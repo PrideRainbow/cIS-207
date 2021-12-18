f33442d64497fcac36eec1d5c43cd2a5543047dc
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2021-12-18 20:06:11 +0000

    removing .idea

--- StockSimulator/Extensions/UIApplication.swift
+++ StockSimulator/Extensions/UIApplication.swift
@@ -2,4 +2,13 @@
 //  UIApplication.swift
 //  StockSimulator
 //
-//  Created by Christopher Walter on 5/18/
+//  Created by Christopher Walter on 5/18/22.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
