540e20632a18bcd51094678d9bb5eb29829d9908
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-01-16 06:13:30 +0000

    updated the demo

--- StockSimulator/Model/Account+CoreDataClass.swift
+++ StockSimulator/Model/Account+CoreDataClass.swift
@@ -73,3 +73,6 @@ public class Account: NSManagedObject {
     //        var theAssets = [Asset]()
     //        if let theTransactionsSet = transactions, let theTransactions = Array(theTransactionsSet) as? [Transaction]
     //        {
+    //            for t in theTransactions {
+    //                // see if I already have asset in the assets
+    //                if let foundAsset = theAssets.first(where: 
