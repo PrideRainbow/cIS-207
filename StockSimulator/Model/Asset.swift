0ef0dc747323c8ff12d17d087fcfe1a32fd69cfc
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-01-25 11:19:59 +0000

    Update options

--- StockSimulator/Model/Asset.swift
+++ StockSimulator/Model/Asset.swift
@@ -2,4 +2,14 @@
 //  Asset.swift
 //  StockSimulator
 //
-//  Created by Christopher W
+//  Created by Christopher Walter on 2/26/22.
+//
+
+import Foundation
+
+
+class Asset: Identifiable, ObservableObject
+{
+    @Published var transactions: [Transaction]
+    @Published var id: UUID
+    @Publi
