79cb8ee659f72ec87863a415a4991c40f4bb1fff
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-02-10 08:25:46 +0000

    debops-optimize

--- StockSimulator/Model/ChartData.swift
+++ StockSimulator/Model/ChartData.swift
@@ -2,4 +2,14 @@
 //  ChartData.swift
 //  StockSimulator
 //
-/
+//  Created by Christopher Walter on 3/22/22.
+//
+
+import Foundation
+
+
+struct MetaData: Codable {
+    var chartPreviousClose: Double
+    var currency: String
+    var dataGranularity: String
+    var exchangeName: 
