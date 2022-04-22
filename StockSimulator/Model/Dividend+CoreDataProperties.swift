e764ac3823b35f8684e45e08fac2effd490c6b47
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-04-22 11:42:43 +0000

    Fix exports

--- StockSimulator/Model/Dividend+CoreDataProperties.swift
+++ StockSimulator/Model/Dividend+CoreDataProperties.swift
@@ -1,2 +1,15 @@
 //
-//  Dividend+CoreDataPropert
+//  Dividend+CoreDataProperties.swift
+//  StockSimulator
+//
+//  Created by Christopher Walter on 7/18/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Dividend {
+
+    @nonobjc public class func fetchRequest() -> NSFet
