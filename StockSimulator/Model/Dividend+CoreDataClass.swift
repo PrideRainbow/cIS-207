422eed9f1e80e1f14798e766ea1922c36c8d7467
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-04-22 09:05:35 +0000

    Rename property

---
+++ StockSimulator/Model/Dividend+CoreDataClass.swift
@@ -0,0 +1,16 @@
+
+//
+//  Dividend+CoreDataClass.swift
+//  StockSimulator
+//
+//  Created by Christopher Walter on 7/18/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Dividend: NSManagedObject {
+
+}
