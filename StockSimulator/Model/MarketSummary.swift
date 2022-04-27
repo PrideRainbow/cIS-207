dada085b237155e3d9d5c64ebef754a6e38b9e5d
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-04-27 16:11:34 +0000

    Fix data race

--- StockSimulator/Model/MarketSummary.swift
+++ StockSimulator/Model/MarketSummary.swift
@@ -8,4 +8,11 @@
 import Foundation
 import SwiftUI
 
-// This file was generated from JSON Schema using quicktype, do not modify i
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try? newJSONDecoder().decode(Welcome.self, from: jsonData)
+
+
+// MARK: - Welcome
+struct C
