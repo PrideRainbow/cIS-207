195cbfd42be6ac0d889e79f7bac8688ebe1f29ff
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-04-27 09:06:30 +0000

    Little improvement

--- StockSimulator/Model/MarketSummary.swift
+++ StockSimulator/Model/MarketSummary.swift
@@ -1,2 +1,11 @@
 //
-//  MarketSum
+//  MarketSummary.swift
+//  StockSimulator
+//
+//  Created by Christopher Walter on 6/13/22.
+//
+
+import Foundation
+import SwiftUI
+
+// This file was generated from JSON Schema using quicktype, do not modify i
