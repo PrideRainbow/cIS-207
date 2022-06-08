51c0884d15a8621fde7fb36f3c7c99fdc2b2876e
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-06-08 21:57:55 +0000

    Fix badges

--- StockSimulator/Model/StatisticModel.swift
+++ StockSimulator/Model/StatisticModel.swift
@@ -13,4 +13,7 @@ struct StatisticModel: Identifiable {
     let title: String
     let value: String
     let percentageChange: Double?
-    let isPercentChange: Bool? // I added this so that I can reuse the view, with a percent s
+    let isPercentChange: Bool? // I added this so that I can reuse the view, with a percent sign or without one. ie can see -12.56 or -12.56%
+    
+    // this will make 2 constructors in one. One constructor has two parameters, the other one has all 3 parameters.
+    init(title: String, va
