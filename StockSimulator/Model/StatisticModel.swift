7f7ed40d8d754d9289a6ecf099569d0e72a695d6
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-06-08 16:13:54 +0000

    change the readme

--- StockSimulator/Model/StatisticModel.swift
+++ StockSimulator/Model/StatisticModel.swift
@@ -9,4 +9,8 @@ import Foundation
 
 struct StatisticModel: Identifiable {
     
-    let id = UUID().uui
+    let id = UUID().uuidString
+    let title: String
+    let value: String
+    let percentageChange: Double?
+    let isPercentChange: Bool? // I added this so that I can reuse the view, with a percent s
