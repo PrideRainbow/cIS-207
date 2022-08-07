96fe3aaf8add96e3b095d51a0ee20246ea6f1866
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-08-07 23:11:05 +0000

    f2fs: trace old block address for CoWed page

--- StockSimulator/Model/Transaction+CoreDataProperties.swift
+++ StockSimulator/Model/Transaction+CoreDataProperties.swift
@@ -10,4 +10,10 @@ import Foundation
 import CoreData
 
 
-extension Transactio
+extension Transaction {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Transaction> {
+        return NSFetchRequest<Transaction>(entityName: "Transaction")
+    }
+
+    @NSManaged public var buyDate: Da
