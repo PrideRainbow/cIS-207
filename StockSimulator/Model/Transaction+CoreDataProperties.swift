34d22a660fe12cf95972d29824ed94f4d836bdca
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-08-09 11:09:54 +0000

    remove unused file

--- StockSimulator/Model/Transaction+CoreDataProperties.swift
+++ StockSimulator/Model/Transaction+CoreDataProperties.swift
@@ -16,4 +16,9 @@ extension Transaction {
         return NSFetchRequest<Transaction>(entityName: "Transaction")
     }
 
-    @NSManaged public var buyDate: Da
+    @NSManaged public var buyDate: Date?
+    @NSManaged public var costBasis: Double
+    @NSManaged public var id: UUID?
+    @NSManaged public var isClosed: Bool
+    @NSManaged public var numShares: Double
+    @NSManaged public var purchas
