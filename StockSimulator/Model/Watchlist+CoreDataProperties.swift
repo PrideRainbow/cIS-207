77ad08289637e57a928329f916923bcdbfe71a31
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-10-16 07:48:00 +0000

    fix silly typo

--- StockSimulator/Model/Watchlist+CoreDataProperties.swift
+++ StockSimulator/Model/Watchlist+CoreDataProperties.swift
@@ -15,4 +15,9 @@ extension Watchlist {
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Watchlist> {
         return NSFetchRequest<Watchlist>(entityName: "Watchlist")
     }
-   
+    @NSManaged public var name: String?
+    @NSManaged public var id: UUID?
+    @NSManaged public var created: Date?
+    @NSManaged public var stocks: NSSet?
+    
+    var wrappedName: Stri
