884d588960031e5788e060e456ce26f55bceb46f
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-10-19 09:22:55 +0000

    travis: update

--- StockSimulator/Persistence.swift
+++ StockSimulator/Persistence.swift
@@ -2,4 +2,13 @@
 //  Persistence.swift
 //  Test
 //
-//  Created by Christopher Wa
+//  Created by Christopher Walter on 6/6/22.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+//    static var preview: PersistenceController = {
+//        let result = PersistenceControll
