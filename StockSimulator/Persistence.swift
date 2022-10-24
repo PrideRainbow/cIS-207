c6dfdbbf35cacc4457af10190c7f72e4eaebbdc5
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-10-24 15:13:57 +0000

    adds travis config

--- StockSimulator/Persistence.swift
+++ StockSimulator/Persistence.swift
@@ -21,4 +21,5 @@ struct PersistenceController {
 //            try viewContext.save()
 //        } catch {
 //            // Replace this implementation with code to handle the error appropriately.
-//           
+//            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//            l
