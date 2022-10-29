96744440640f41a31beb2853c03d21da3b3c3dd5
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-10-29 19:52:03 +0000

    Updated twitterstats

--- StockSimulator/Persistence.swift
+++ StockSimulator/Persistence.swift
@@ -43,4 +43,7 @@ struct PersistenceController {
                 /*
                 Typical reasons for an error here include:
                 * The parent directory does not exist, cannot be created, or disallows writing.
-                * The persistent store is not accessible, due to permissions or data 
+                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                * The device is out of space.
+                * The store could not be migrated to the current model version.
+                Check the error messag
