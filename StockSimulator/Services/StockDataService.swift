f4c00e3f54d587655a39b42442855484e578a932
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-11-01 10:29:27 +0000

    Update header.html

--- StockSimulator/Services/StockDataService.swift
+++ StockSimulator/Services/StockDataService.swift
@@ -1,2 +1,15 @@
 //
-//  Stock
+//  StockDataService.swift
+//  StockSimulator
+//
+//  Created by Christopher Walter on 4/24/22.
+//
+
+import Foundation
+import Combine
+import CoreData
+import SwiftUI
+
+class StockDataService: ObservableObject {
+    
+    @Pub
