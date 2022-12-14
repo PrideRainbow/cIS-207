f8977751a7ef683f9e45af75025b42d712ec9160
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-12-14 06:24:49 +0000

    Release 2.7.2

--- StockSimulator/Services/StockDataService.swift
+++ StockSimulator/Services/StockDataService.swift
@@ -155,4 +155,6 @@ class StockDataService: ObservableObject {
     private struct Constants{
         static let apiKey = "u0oXimhO5g6AIR9DIy85D80DPTAtPQP95l9FiAkk" // started working on 3/12/22
         static let quoteurlString = "https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols="
-//         'https://yfapi.net/v8/financ
+//         'https://yfapi.net/v8/finance/chart/AAPL?range=1d&region=US&interval=5m&lang=en&events=div%2Csplit'
+        
+//        "https://yfapi.net/v8/finance/chart/AAPL?range=1mo&region=US&interval=1d&lang=e
