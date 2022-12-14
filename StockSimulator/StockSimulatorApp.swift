747568883282c58e2c14fdfee60a4fc8d28021d5
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2022-12-14 08:58:04 +0000

    New translations strings.xml (Japanese)

--- StockSimulator/StockSimulatorApp.swift
+++ StockSimulator/StockSimulatorApp.swift
@@ -2,3 +2,45 @@
 //
 //  StockSimulatorApp.swift
 //  StockSimulator
+//
+//  Created by Christopher Walter on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct StockSimulatorApp: App {
+//    let persistenceController = PersistenceController.shared
+
+    @StateObject private var dataController: DataController = DataController()
+//    @StateObject private var vm: AccountViewModel
+    @StateObject private var stockVM = StocksViewModel()
+    @StateObject private var marketSummaryVM = MarketSummaryViewModel()
+    
+//    init(){
+//        let dataController = DataController()
+//        _dataController = StateObject(wrappedValue: dataController) // core Data from 100 days SwiftUI Project 11
+////        _vm = StateObject(wrappedValue: AccountViewModel(account: Account(context: dataController.container.viewContext)))
+//    }
+    
+    @State private var showLaunchView: Bool = true
+    
+    var body: some Scene {
+        WindowGroup {
+            ZStack {
+                ContentView()
+                    .environment(\.managedObjectContext, dataController.container.viewContext)
+                    .environmentObject(stockVM)
+                    .environmentObject(marketSummaryVM)
+    //                .environmentObject(dataController)
+    //                .environmentObject(vm)
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ StockSimulator/Utilities/HapticManager.swift
@@ -0,0 +1,11 @@
+//
+//  HapticManager.swift
+//  StockSimulator
+//
+//  Created by Christopher Walter on 8/2/22.
+//
+
+import Foundation
+import SwiftUI
+
+class Hapti
