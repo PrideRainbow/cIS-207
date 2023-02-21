f8ede551313ec4258d76268a736cff4ff2135230
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2023-02-21 20:13:00 +0000

    debian: update changelog

--- StockSimulatorUITests/StockSimulatorUITestsLaunchTests.swift
+++ StockSimulatorUITests/StockSimulatorUITestsLaunchTests.swift
@@ -20,3 +20,8 @@ class StockSimulatorUITestsLaunchTests: XCTestCase {
     func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let
