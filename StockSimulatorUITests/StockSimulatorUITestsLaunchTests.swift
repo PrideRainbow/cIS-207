23185472fa8412d7262643cfdc58b99a4e456ecf
PrideRainbow <115651420+PrideRainbow@users.noreply.github.com>
2023-02-21 21:54:15 +0000

    Updated Icons

--- StockSimulatorUITests/StockSimulatorUITestsLaunchTests.swift
+++ StockSimulatorUITests/StockSimulatorUITestsLaunchTests.swift
@@ -24,4 +24,9 @@ class StockSimulatorUITestsLaunchTests: XCTestCase {
         // Insert steps here to perform after app launch but before taking a screenshot,
         // such as logging into a test account or navigating somewhere in the app
 
-        let
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
